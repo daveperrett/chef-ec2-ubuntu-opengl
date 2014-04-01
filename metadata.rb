9eb2bb2378ebaf606c139be1758f7259c0daf580
Dave Perrett <hello@daveperrett.com>
2014-04-01 23:27:40 +1100

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-ec2-ubuntu-opengl'
+maintainer       'Dave Perrett'
+maintainer_email 'hello@daveperrett.com'
+license          'MIT'
+description      'Sets up OpenGL on an EC2 ubuntu GPU instance.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef-ec2-ubuntu-opengl
+# Recipe:: default
+#
+# Copyright 2014, Dave Perrett
+#
+# Licensed under http://opensource.org/licenses/MIT
+#
