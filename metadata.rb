eae1335d21192b292e577d6c3c73abaf42d03d73
Dave Perrett <hello@daveperrett.com>
2014-04-02 00:50:15 +1100

    Add basic package installation and configuration.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
-name             'chef-ec2-ubuntu-opengl'
+name             'ec2-ubuntu-opengl'
 maintainer       'Dave Perrett'
 maintainer_email 'hello@daveperrett.com'
 license          'MIT'
 description      'Sets up OpenGL on an EC2 ubuntu GPU instance.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'ubuntu'
+
+depends 'apt'
+
+recipe 'ec2-ubuntu-opengl', 'Sets up OpenGL on an EC2 ubuntu GPU instance.'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,30 @@
 #
 # Licensed under http://opensource.org/licenses/MIT
 #
+
+include_recipe 'apt'
+
+# Add the x-updates repository.
+apt_repository 'ubuntu-x-swat-x-updates-ppa' do
+  uri 'http://ppa.launchpad.net/ubuntu-x-swat/x-updates/ubuntu/'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key 'AF1CDFA9'
+  action :add
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+# Install packages.
+PACKAGES = %w(nvidia-current xserver-xorg libglu1-mesa-dev freeglut3-dev mesa-common-dev libxmu-dev libxi-dev)
+
+PACKAGES.each do |package|
+  apt_package package do
+    action :install
+  end
+end
+
+execute 'configure nvidia' do
+  command 'nvidia-xconfig -a --use-display-device=None --virtual=1280x1024'
+  not_if { ::File.exists?('/etc/X11/xorg.conf')}
+end
